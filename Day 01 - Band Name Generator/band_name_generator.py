44d36a02fabcd3dae654384e4e369e8133dce3bf
Prasath D.V <prasath.dv@gmail.com>
2020-11-11 00:26:05 +0530

    Day 01 - Nov 10 - Band Name Generator Module

---
+++ Day 01 - Band Name Generator/band_name_generator.py
@@ -0,0 +1,6 @@
+# This code will generate random band names for your local bands.
+
+city_name = input('What`s the name of the city you grew up in?')
+pet_name = input('What`s your pet name?')
+
+print('Your band name could be ' + city_name + ' ' + pet_name)
